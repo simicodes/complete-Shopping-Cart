b07b2536ff66121dfcd4d488a33eed217c517cbe
simicodes <simeonoffiong@gmail.com>
2022-07-28 10:36:27 +0100

    Complete online shopping cart

---
+++ backend/products.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    desc: "6.1-inch display",
+    price: 999,
+    image:
+      "https://res.cloudinary.com/aptive-technologies/image/upload/v1658593287/online-shop/iphone12pro_hdw1f6.jpg",
+  },
+  {
+    id: 2,
+    name: "iPhone 12 ",
+    desc: "5.4-inch display",
+    price: 699,
+    image:
+      "https://res.cloudinary.com/aptive-technologies/image/upload/v1658593287/online-shop/iphone12_yih3zf.jpg",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/aptive-technologies/image/upload/v1658593287/online-shop/galaxyS_zsrq51.png",
+  },
+];
+
+module.exports = products;
